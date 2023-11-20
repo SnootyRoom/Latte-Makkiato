1d646b4d480dd5a3053cc534e5da40ae7f22921e
Bogdan <snootyroom25@gmail.com>
2023-11-20 22:08:19 +0500

    ok

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import sys
 import random
+import sys
 
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtCore import QPointF
 from PyQt5 import uic
 
 
 class YellowCircles(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui')
+        uic.loadUi('UI.ui', self)
 
         self.btn.clicked.connect(self.paint)
         self.do_paint = False
@@ -18,7 +17,7 @@ class YellowCircles(QMainWindow):
     def paintEvent(self, event):
         if self.do_paint:
             qp = QPainter()
-            qp.begin()
+            qp.begin(self)
             self.drawCircle(qp)
             qp.end()
         self.do_paint = False
@@ -28,10 +27,12 @@ class YellowCircles(QMainWindow):
         self.update()
 
     def drawCircle(self, qp):
-        qp.setBrush(QColor(255, 165, 0))
-        r = random.randint(50, 250)
-        c = random.randint(20, 300), random.randint(20, 300)
-        qp.drawEllipse(QPointF(c), r)
+        qp.setBrush(QColor(255, 255, 0))
+        x = random.randint(20, 520)
+        y = random.randint(20, 520)
+        R = random.randint(50, 350)
+        qp.drawEllipse(int(x - R / 2),
+                       int(y - R / 2), R, R)
 
 
 if __name__ == '__main__':
