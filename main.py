33602f1253182dce2923878f6a176e1ced651050
Bogdan <snootyroom25@gmail.com>
2023-11-21 22:52:42 +0500

    ok

--- main.py
+++ main.py
@@ -1,43 +1,33 @@
-import random
 import sys
+import sqlite3
 
-from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from UI import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5 import uic
 
 
-class RandomCircles(QMainWindow, Ui_MainWindow):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
+        uic.loadUi('main.ui', self)
+        self.con = sqlite3.connect('coffee.sqlite')
+        self.showTable()
 
-        self.btn.clicked.connect(self.paint)
-        self.do_paint = False
+    def showTable(self):
+        cur = self.con.cursor()
+        res = cur.execute('''SELECT * FROM Coffee''').fetchall()
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
 
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.drawCircle(qp)
-            qp.end()
-        self.do_paint = False
-
-    def paint(self):
-        self.do_paint = True
-        self.update()
-
-    def drawCircle(self, qp):
-        r, g, b = random.randint(0,255), random.randint(0,255), random.randint(0,255)
-        qp.setBrush(QColor(r, g, b))
-        x = random.randint(20, 520)
-        y = random.randint(20, 520)
-        R = random.randint(50, 350)
-        qp.drawEllipse(int(x - R / 2),
-                       int(y - R / 2), R, R)
+    def closeEvent(self, event):
+        self.con.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = RandomCircles()
+    ex = MyWidget()
     ex.show()
     sys.exit(app.exec())
