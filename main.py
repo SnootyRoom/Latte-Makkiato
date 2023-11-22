964c5e12da6f0d9ab20f68abe80aa066ad6c8fff
Bogdan <snootyroom25@gmail.com>
2023-11-22 21:30:08 +0500

    ok

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ from PyQt5 import uic
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
+        uic.loadUi('UI.ui', self)
         self.con = sqlite3.connect('coffee.sqlite')
         self.showTable()
+        self.upd_btn.clicked.connect(self.showTable)
 
     def showTable(self):
         cur = self.con.cursor()
@@ -26,8 +27,56 @@ class MyWidget(QMainWindow):
         self.con.close()
 
 
+class AddChangeCoffee(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+
+        self.con = sqlite3.connect('coffee.sqlite')
+        self.add_btn.clicked.connect(self.addCoffee)
+        self.change_btn.clicked.connect(self.changeCoffee)
+        self.my_widget = MyWidget()
+
+    def addCoffee(self):
+        cur = self.con.cursor()
+        name = self.name.text()
+        roasting = self.roasting.text()
+        structure = self.structure.text()
+        descreption = self.descreption.text()
+        price = int(self.price.text())
+        amount = int(self.amount.text())
+        cur.execute('''INSERT INTO Coffee(name, roasting, structure, descreption, price, amount) 
+        VALUES(?, ?, ?, ?, ?, ?)''', (name, roasting, structure, descreption, price, amount,))
+        self.con.commit()
+        self.my_widget.showTable()
+
+    def changeCoffee(self):
+        cur = self.con.cursor()
+        change = self.changeline.text()
+        id = int(self.id.text())
+        if self.comboBox.currentText() == 'Название':
+            cur.execute('''UPDATE Coffee SET name = ? WHERE id = ?''', (change, id,))
+        if self.comboBox.currentText() == 'Обжарка':
+            cur.execute('''UPDATE Coffee SET roasting = ? WHERE id = ?''', (change, id,))
+        if self.comboBox.currentText() == 'Структура':
+            cur.execute('''UPDATE Coffee SET structure = ? WHERE id = ?''', (change, id,))
+        if self.comboBox.currentText() == 'Описание':
+            cur.execute('''UPDATE Coffee SET descreption = ? WHERE id = ?''', (change, id,))
+        if self.comboBox.currentText() == 'Цена':
+            cur.execute('''UPDATE Coffee SET price = ? WHERE id = ?''', (int(change), id,))
+        if self.comboBox.currentText() == 'Объём':
+            cur.execute('''UPDATE Coffee SET amount = ? WHERE id = ?''', (int(change), id,))
+        self.con.commit()
+        self.my_widget.showTable()
+
+    def closeEvent(self, event):
+        self.con.close()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
     ex.show()
+    e = AddChangeCoffee()
+    e.show()
     sys.exit(app.exec())
